84ab0d9173072875a0afc57f709906d4f000601e
Quentin PAVY <quentin.pavy@orange.fr>
2014-03-11 18:11:35 +0100

    [Controller] Ajout du premier controller pour la home

--- src/M2TIIL/TripBookerBundle/Controller/DefaultController.php
+++ src/M2TIIL/TripBookerBundle/Controller/DefaultController.php
@@ -3,11 +3,18 @@
 namespace M2TIIL\TripBookerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+	/**
+	 * @Route("/", name="tripBooker_home")
+	 */
+    public function indexAction()
     {
-        return $this->render('M2TIILTripBookerBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('M2TIILTripBookerBundle::index.html.twig', array(
+        	'packs' => array(),
+        	'form_custom_cities' => array(), 
+        ));
     }
 }
