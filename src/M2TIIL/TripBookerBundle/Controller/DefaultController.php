84c4a7bc7e414441dc50852171855512c3a61edb
kvasir35 <yohann.person@hotmail.fr>
2014-03-31 23:46:03 +0200

    test person commit

--- src/M2TIIL/TripBookerBundle/Controller/DefaultController.php
+++ src/M2TIIL/TripBookerBundle/Controller/DefaultController.php
@@ -16,6 +16,7 @@ class DefaultController extends Controller
 	 */
     public function indexAction()
     {
+		$packs = array();
 		$em = $this->getDoctrine()->getManager();
 		$packs = $em->getRepository('M2TIILTripBookerBundle:Trip')->findAll();
 		
