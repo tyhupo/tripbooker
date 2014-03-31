fddd4db8265168b06f00b01887b3437fd2a5bade
Quentin PAVY <quentin.pavy@orange.fr>
2014-03-31 18:48:23 +0200

    [FRONT] Ajout css home + controller

--- src/M2TIIL/TripBookerBundle/Controller/DefaultController.php
+++ src/M2TIIL/TripBookerBundle/Controller/DefaultController.php
@@ -17,4 +17,24 @@ class DefaultController extends Controller
         	'form_custom_cities' => array(), 
         ));
     }
+
+    /**
+     * @Route("/hotels/", name="tripbooker_hotels_list")
+     */
+    public function hotelAction()
+    {
+    	return $this->render('M2TIILTripBookerBundle:Hotels:hotels.html.twig',array(
+    		'hotels' => array(),
+		));
+    }
+
+    /**
+     * @Route("/excursions/", name="tripbooker_excursions_list")
+     */
+    public function excursionAction()
+    {
+    	return $this->render('M2TIILTripBookerBundle:Excursions:excursions.html.twig',array(
+    		'excursions' => array(),
+		));
+    }
 }
