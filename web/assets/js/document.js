929b116e4132b203c681f28d6cad5799bc833d4e
Quentin PAVY <quentin.pavy@orange.fr>
2014-04-01 00:01:56 +0200

    [FRONT] ajout slider header

--- web/assets/js/document.js
+++ web/assets/js/document.js
@@ -1,3 +1,13 @@
 jQuery(document).ready(function($) {
-	
+	$("#header-slider").owlCarousel({
+    	autoPlay: 5000, //Set AutoPlay to 3 seconds
+ 		slideSpeed: 1000,
+      	items : 1,
+      	itemsDesktop : [1920,1]
+	});
+
+	$("#packs-slider").owlCarousel({
+		items : 3,
+		itemsDesktop : [1920,3]
+	});
 });
