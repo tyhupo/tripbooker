45844d0e3261d303fa193cb0385c00b2ca3a6ae7
Quentin PAVY <quentin.pavy@orange.fr>
2014-04-02 06:45:24 +0200

    [FRONT] ajout fiche voyage et fiche etape

--- src/M2TIIL/TripBookerBundle/Controller/ProductController.php
+++ src/M2TIIL/TripBookerBundle/Controller/ProductController.php
@@ -11,54 +11,95 @@ use M2TIIL\TripBookerBundle\Entity\GuidedTour;
 class ProductController extends Controller
 {
 	/**
-	 * @Route("/fiche-voyage", name="fiche-voyage")
+	 * @Route("/fiche-voyage/{id}", name="fiche-voyage")
 	 */
 	public function voyageAction($id)
 	{
 		$em = $this->getDoctrine()->getManager();
 		$voyage = $em->getRepository('M2TIILTripBookerBundle:Trip')->find($id);
 		
+
+		$packs_trip = $em->getRepository('M2TIILTripBookerBundle:TripStep')->findAll();
+		$tab_trip_startCity = array(); 
+		$tab_trip_endCity = array(); 
+		foreach($packs_trip as $p){
+			$tab_trip_startCity[$p->getId()]=array($p->getStartCity());
+			$tab_trip_endCity[$p->getId()]=array($p->getEndCity());
+		}
+
 		return $this->render('M2TIILTripBookerBundle:FicheVoyage:fiche-voyage.html.twig', array(
-        	'value' => $voyage,
+        	'voyage' => $voyage,
+        	'form_custom_Startcities' => $tab_trip_startCity,
+        	'form_custom_Endcities' => $tab_trip_endCity,
         ));
 	}
 	
 	/**
-	 * @Route("/fiche-etape", name="fiche-etape")
+	 * @Route("/fiche-etape/{id}", name="fiche-etape")
 	 */
 	public function etapeAction($id)
 	{
 		$em = $this->getDoctrine()->getManager();
 		$etape = $em->getRepository('M2TIILTripBookerBundle:Trip')->find($id);
 		
+		$packs_trip = $em->getRepository('M2TIILTripBookerBundle:TripStep')->findAll();
+		$tab_trip_startCity = array(); 
+		$tab_trip_endCity = array(); 
+		foreach($packs_trip as $p){
+			$tab_trip_startCity[$p->getId()]=array($p->getStartCity());
+			$tab_trip_endCity[$p->getId()]=array($p->getEndCity());
+		}
+
 		return $this->render('M2TIILTripBookerBundle:FicheEtape:fiche-etape.html.twig', array(
-        	'value' => $etape,
+        	'etape' => $etape,
+        	'form_custom_Startcities' => $tab_trip_startCity,
+        	'form_custom_Endcities' => $tab_trip_endCity,
         ));
 	}
 	
 	/**
-	 * @Route("/fiche-excursion", name="fiche-excursion")
+	 * @Route("/fiche-excursion/{id}", name="fiche-excursion")
 	 */
 	public function excursionAction($id)
 	{
 		$em = $this->getDoctrine()->getManager();
 		$excursion = $em->getRepository('M2TIILTripBookerBundle:Trip')->find($id);
 		
+		$packs_trip = $em->getRepository('M2TIILTripBookerBundle:TripStep')->findAll();
+		$tab_trip_startCity = array(); 
+		$tab_trip_endCity = array(); 
+		foreach($packs_trip as $p){
+			$tab_trip_startCity[$p->getId()]=array($p->getStartCity());
+			$tab_trip_endCity[$p->getId()]=array($p->getEndCity());
+		}
+
 		return $this->render('M2TIILTripBookerBundle:FicheExcursion:fiche-excursion.html.twig', array(
         	'value' => $excursion,
+        	'form_custom_Startcities' => $tab_trip_startCity,
+        	'form_custom_Endcities' => $tab_trip_endCity,
         ));
 	}
 	
 	/**
-	 * @Route("/fiche-hotel", name="fiche-hotel")
+	 * @Route("/fiche-hotel/{id}", name="fiche-hotel")
 	 */
 	public function hotelAction($id)
 	{
 		$em = $this->getDoctrine()->getManager();
 		$hotel = $em->getRepository('M2TIILTripBookerBundle:Trip')->find($id);
 		
+		$packs_trip = $em->getRepository('M2TIILTripBookerBundle:TripStep')->findAll();
+		$tab_trip_startCity = array(); 
+		$tab_trip_endCity = array(); 
+		foreach($packs_trip as $p){
+			$tab_trip_startCity[$p->getId()]=array($p->getStartCity());
+			$tab_trip_endCity[$p->getId()]=array($p->getEndCity());
+		}
+
 		return $this->render('M2TIILTripBookerBundle:FicheHotel:fiche-hotel.html.twig', array(
         	'value' => $hotel,
+        	'form_custom_Startcities' => $tab_trip_startCity,
+        	'form_custom_Endcities' => $tab_trip_endCity,
         ));
 	}
 }
